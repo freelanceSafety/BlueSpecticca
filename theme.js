d7c61f8e451e9112fd828c341dad92a1a60a5d9b
Rohit-work <91243313+Rohit-work@users.noreply.github.com>
2021-11-24 15:14:05 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -2,3 +2,7 @@ console.log("Website by Rohit Parakh IG: @rohcodes")
 if(window.location.hostname.toLowerCase().includes('luxewalk')){
  console.log("Hi Luxewalk") 
 }
+
+if(window.location.hostname.toLowerCase().includes('teenpreneur')){
+ console.log("Hi teenpreneur") 
+}
