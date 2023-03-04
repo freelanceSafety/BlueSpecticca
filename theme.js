1fb0c78f68b971a3195d8af741548b90d5449bbb
freelanceSafety <91243313+freelanceSafety@users.noreply.github.com>
2023-03-04 10:40:35 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -13,3 +13,7 @@ if(window.location.hostname.toLowerCase().includes('onset')){
 if(window.location.hostname.toLowerCase().includes('bombaytoycompany')){
  console.log("Hi BTC") 
 }
+
+if(window.location.hostname.toLowerCase().includes('bumberry')){
+ console.log("Hi Bumberry") 
+}
