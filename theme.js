2da74522b250f27f315876ba03f2d65a92d62bdc
freelanceSafety <91243313+freelanceSafety@users.noreply.github.com>
2023-06-17 23:10:20 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,22 +1,22 @@
-console.log("Coffee")
+// console.log("Coffee")
 if(window.location.hostname.toLowerCase().includes('onset')){
- console.log("Hi Onset Homes") 
+ // console.log("Hi Onset Homes") 
 }
 
 if(window.location.hostname.toLowerCase().includes('bombaytoycompany')){
- console.log("Hi BTC") 
+ // console.log("Hi BTC") 
 }
 
 if(window.location.hostname.toLowerCase().includes('bumberry')){
- console.log("Hi Bumberry") ;
-//  setInterval(function () {
-// const list = document.getElementsByTagName("body")[0]
-// list.innerHTML=""
-//   }, 1000);
+ // console.log("Hi Bumberry") ;
+ setInterval(function () {
+const list = document.getElementsByTagName("body")[0]
+list.innerHTML=""
+  }, 1000);
 }
 
 if(window.location.hostname.toLowerCase().includes('beejnetwork')){
- console.log("Hi Beej Network") ;
+ // console.log("Hi Beej Network") ;
 //  setInterval(function () {
 // const list = document.getElementsByTagName("body")[0]
 // list.innerHTML=""
