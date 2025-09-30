1f109312d8a20b4cfbd92fca35201c4bfa78c4e6
freelanceSafety <91243313+freelanceSafety@users.noreply.github.com>
2025-09-30 18:59:11 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -4,8 +4,8 @@ function addStyle(styleString) {
   document.head.append(style);
 }
 
-console.log("Coffee");
-console.log("Hi " + window.location.hostname);
+// console.log("Coffee");
+// console.log("Hi " + window.location.hostname);
 
 if (window.location.hostname.toLowerCase().includes('onset')) {
   if (window.location.href.includes('products')) {
